fe6be9a927bd7be7ebc48012d1cc60d0b77bef07
Fede <federicocaballero7@gmail.com>
2024-07-09 10:23:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('calculatorForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const distanceInput = document.getElementById('distance').value.replace(',', '.');
+    const distance = parseFloat(distanceInput);
+    const hours = parseInt(document.getElementById('hours').value);
+    const minutes = parseInt(document.getElementById('minutes').value);
+    const seconds = parseInt(document.getElementById('seconds').value);
+    
+    if (isNaN(distance) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
+        document.getElementById('result').innerText = 'Por favor, introduce valores válidos.';
+        return;
+    }
+
+    const totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+    const paceSeconds = totalSeconds / distance;
+    
+    const paceMinutes = Math.floor(paceSeconds / 60);
+    const remainingSeconds = Math.floor(paceSeconds % 60);
+    
+    document.getElementById('result').innerText = `Ritmo: ${paceMinutes} minutos ${remainingSeconds} segundos por km`;
+});
+
+function setDistance(value) {
+    document.getElementById('distance').value = value;
+}
